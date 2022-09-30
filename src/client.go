e7e67d36a561670d15cee8133cd38da4f2891795
Florian Buetler <flbuetle@ethz.ch>
2022-09-30 19:05:09 +0200

    add directory, account, order, authorization and challenge objects

--- src/client.go
+++ src/client.go
@@ -10,6 +10,100 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+type orderStatus int
+type authorizationStatus int
+type challengeStatus int
+type accountStatus int
+
+const (
+	ORDER_PENDING orderStatus = iota
+	ORDER_READY
+	ORDER_PROCESSING
+	ORDER_VALID
+	ORDER_INVALID
+)
+
+const (
+	AUTHORIZATION_PENDING authorizationStatus = iota
+	AUTHORIZATION_VALID
+	AUTHORIZATION_INVALID
+	AUTHORIZATION_DEACTIVATED
+	AUTHORIZATION_EXPIRED
+	AUTHORIZATION_REVOKED
+)
+
+const (
+	CHALLENGE_PENDING challengeStatus = iota
+	CHALLENGE_PROCESSING
+	CHALLENGE_VALID
+	CHALLENGE_INVALID
+)
+
+const (
+	ACCOUNT_VALID accountStatus = iota
+	ACCOUNT_DEACTIVATED
+	ACCOUNT_REVOKED
+)
+
+// In order to help clients configure themselves with the right URLs for
+// each ACME operation, ACME servers provide a directory object.
+type dir struct {
+	KeyChangeURL  string `json:"keyChange"`
+	NewAccountURL string `json:"newAccount"`
+	NewNonceURL   string `json:"newNonce"`
+	NewOrderURL   string `json:"newOrder"`
+	RevokeCertURL string `json:"revokeCert"`
+}
+
+// An ACME account resource represents a set of metadata associated with an account.
+type account struct {
+	Status    accountStatus `json:"status"`
+	OrdersURL []string      `json:"orders"`
+}
+
+// Each account object includes an "orders" URL from which a list of
+// orders created by the account can be fetched via POST-as-GET request.
+// The result of the request MUST be a JSON object whose "orders" field
+// is an array of URLs, each identifying an order belonging to the
+// account.
+type ordersList struct {
+	Orders []string `json:"orders"`
+}
+
+// An ACME order object represents a client's request for a certificate
+// and is used to track the progress of that order through to issuance.
+type order struct {
+	Status            orderStatus  `json:"status"`
+	Identifiers       []identifier `json:"identifiers"`
+	AuthorizationURLs []string     `json:"authorizations"`
+	FinalizeURL       string       `json:"finalize"`
+	CertificateURL    string       `json:"certificate"`
+	// expires
+	// notBefore
+	// notAfter
+}
+
+type identifier struct {
+	Type   string `json:"type"` // usually 'dns'
+	Values string `json:"value"`
+}
+
+// An ACME authorization object represents a server's authorization for
+// an account to represent an identifier.
+type authorization struct {
+	Identifier identifier          `json:"identifier"`
+	Status     authorizationStatus `json:"status"`
+	Challenges []challenge         `json:"challenges"`
+	Wildcard   bool                `json:"wildcard"`
+	// expires
+}
+
+// An ACME challenge object represents a server's offer to validate a
+// client's possession of an identifier in a specific way.
+type challenge struct {
+	// content depends on challenge TODO -> RFC Section 8
+}
+
 type client struct {
 	httpClient    http.Client
 	directoryURL  string
